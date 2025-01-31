c7216b78e9d0a111703f4123badb5c228790f3d0
constantin9845 <constantin.y@outlook.com>
2025-01-31 00:24:07 -0800

    fix windows upload bug

---
+++ electron-app/scripts/ui_win.js
@@ -0,0 +1,56 @@
+const { ipcRenderer } = require('electron');
+
+const selectFilesBtn = document.getElementById('selectFilesBtn');
+const fileList = document.getElementById('fileList');
+
+const selectDirBtn = document.getElementById('selectDirsBtn');
+const dirList = document.getElementById('dirList');
+
+
+// File Seletion 
+selectFilesBtn.addEventListener('click', async () => {
+    const filePaths = await ipcRenderer.invoke('select-files'); // Request file selection
+
+    fileList.innerHTML = ""; // Clear previous file list
+    fileCollection = [];
+
+    if (filePaths && filePaths.length > 0) {
+        filePaths.forEach(path => {
+            const listItem = document.createElement('li');
+            listItem.textContent = path; // Display each selected file path
+            fileList.appendChild(listItem);
+            fileCollection.push(path);
+        });
+    } 
+    else {
+        const noFiles = document.createElement('li');
+        noFiles.textContent = 'No files selected.';
+        fileList.appendChild(noFiles);
+    }
+});
+
+
+// Directory selection WINDOWS
+selectDirBtn.addEventListener('click', async ()=>{
+    const dirPaths = await ipcRenderer.invoke('select-dirs');
+
+    dirList.innerHTML = ""; // Clear previous file list
+    fileCollection = [];
+
+    if(dirPaths && dirPaths.length > 0){
+        dirPaths.forEach(path =>{
+            const listItem = document.createElement('li');
+            listItem.textContent = path; // Display each selected file path
+            dirList.appendChild(listItem);
+            fileCollection.push(path);
+        })
+    }
+})
+
+
+// send file paths
+const encBtn = document.getElementById('encBtn');
+
+encBtn.addEventListener('click',async ()=>{
+    ipcRenderer.send('path-collection', fileCollection);
+})
